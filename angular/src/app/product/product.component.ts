a1a5524f517614b9579d8cc50b1acaf9524b0618
Toan bach <ngoctoan@live.com>
2022-11-09 15:53:33 +0700

    feat: Loading data to product page and paging

--- angular/src/app/product/product.component.ts
+++ angular/src/app/product/product.component.ts
@@ -1,22 +1,53 @@
-import { AuthService } from '@abp/ng.core';
-import { Component } from '@angular/core';
-import { OAuthService } from 'angular-oauth2-oidc';
+import { PagedResultDto } from '@abp/ng.core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ProductInListDto, ProductsService } from '@proxy/products';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss'],
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit, OnDestroy {
+  private ngUnsubscribe = new Subject<void>();
   blockedPanel: boolean = false;
-  items = [];
-  get hasLoggedIn(): boolean {
-    return this.oAuthService.hasValidAccessToken();
+  items: ProductInListDto[] = [];
+
+  //Paging variables
+  public skipCount: number = 0;
+  public maxResultCount: number = 10;
+  public totalCount: number;
+
+  constructor(private productService: ProductsService) {}
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
   }
 
-  constructor(private oAuthService: OAuthService, private authService: AuthService) {}
+  ngOnInit(): void {
+    this.loadData();
+  }
 
-  login() {
-    this.authService.navigateToLogin();
+  loadData() {
+    this.productService
+      .getListFilter({
+        keyword: '',
+        maxResultCount: this.maxResultCount,
+        skipCount: this.skipCount,
+      })
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe({
+        next: (response: PagedResultDto<ProductInListDto>) => {
+          this.items = response.items;
+          this.totalCount = response.totalCount;
+        },
+        error: () => {},
+      });
+  }
+
+  pageChanged(event: any): void {
+    this.skipCount = (event.page -1) * this.maxResultCount;
+    this.maxResultCount = event.rows;
+    this.loadData();
   }
 }
