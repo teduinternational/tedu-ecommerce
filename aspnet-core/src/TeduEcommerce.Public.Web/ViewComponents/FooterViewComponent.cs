04b4d08a9987a770835c0b1d125ab53234fdbb2d
Toan bach <ngoctoan@live.com>
2023-01-07 05:07:28 +0700

    feat: Add header and footer view components

---
+++ aspnet-core/src/TeduEcommerce.Public.Web/ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace TeduEcommerce.Public.Web.ViewComponents
+{
+    public class FooterViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
---
+++ aspnet-core/src/TeduEcommerce.Public.Web/ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace TeduEcommerce.Public.Web.ViewComponents
+{
+    public class HeaderViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
