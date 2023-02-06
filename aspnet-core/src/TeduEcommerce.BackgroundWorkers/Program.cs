59e36cef33756c66b6487bd055bae9131037e659
Toan bach <ngoctoan@live.com>
2023-02-06 14:59:37 +0700

    feat: Add background worker

---
+++ aspnet-core/src/TeduEcommerce.BackgroundWorkers/Program.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+
+namespace TeduEcommerce.BackgroundWorkers;
+
+public class Program
+{
+    public static IConfigurationRoot Configuration { get; private set; }
+    public async static Task<int> Main(string[] args)
+    {
+        Log.Logger = new LoggerConfiguration()
+#if DEBUG
+            .MinimumLevel.Debug()
+#else
+            .MinimumLevel.Information()
+#endif
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
+            .Enrich.FromLogContext()
+            .WriteTo.Async(c => c.File("Logs/logs.txt"))
+            .WriteTo.Async(c => c.Console())
+            .CreateLogger();
+
+        try
+        {
+            Log.Information("Starting console host.");
+            await CreateHostBuilder(args).RunConsoleAsync();
+            return 0;
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "Host terminated unexpectedly!");
+            return 1;
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+    }
+
+    internal static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostContext, configBuilder) =>
+                {
+                    configBuilder.Sources.Clear();
+                    configBuilder
+                            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+                    IConfigurationRoot configurationRoot = configBuilder.Build();
+
+                    Configuration = configurationRoot;
+                })
+                .UseAutofac()
+                .UseSerilog()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddApplication<TeduEcommerceBackgroundWorkersModule>();
+                });
+}
+
